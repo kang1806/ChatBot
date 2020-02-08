1ce483d8afbe1e9d4b339f745923342ae0a51c3a
kang1806 <40084751+kang1806@users.noreply.github.com>
2020-02-08 18:58:20 +0800

    Training Write to Database

--- app.py
+++ app.py
@@ -2,32 +2,49 @@ from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
 from flask import Flask, render_template, request
 
+import pymongo
+
 app = Flask(__name__)
 
 english_bot = ChatBot("Chatterbot", storage_adapter="chatterbot.storage.SQLStorageAdapter")
 trainer = ChatterBotCorpusTrainer(english_bot)
 trainer.train("chatterbot.corpus.english")
 
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = myclient["chatbot"]
+mycol = mydb["training"]
+
 @app.route("/")
 def home():
     return render_template("index.html")
-    
+
 @app.route("/training")
 def training():
     return render_template("training.html")
-    
 
-@app.route("/get")
+@app.route("/getTrain")
+def get_user_train():
+        userTrain = request.args.get('msg')
+        print (userTrain)
+        mydict = { "Msg": userTrain }
+        x = mycol.insert_one(mydict)
+        return str(userTrain)
+
+
+
+@app.route("/getResponse")
 def get_bot_response():
     userText = request.args.get('msg')
+    print (userText)
     bot_response = english_bot.get_response(userText)
     if bot_response.confidence > 0.5:
         return str(bot_response)
     else:
         return str("I dont understand this")
-        
+
 
 
 
 if __name__ == "__main__":
+    app.debug = True
     app.run()
